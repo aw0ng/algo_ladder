fad90d0ec58991b40aa5117ea2c8467456de7995
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-08-05 23:48:34 -0400

    select_less_than function

--- Basic Array/select_less_than_100.rb
+++ Basic Array/select_less_than_100.rb
@@ -4,10 +4,32 @@
 # Output: [99, 88, 4, 50]
 
 # CLARIFY
+# array = [13, 113, 3, 33, 1133, 31]
+# new_array = [13, 3, 33, 31]
 
 # VISUALIZE
+# array = [13, 113, 3, 33, 1133, 31]
+            # i
+            # if i < 100
+            #   new array < i
+# new_array = [13, 3, 33, 31]
 
 # CODE
+
+def less_than(numbers)
+  i = 0
+  new_numbers = []
+  while i < numbers.length
+    if numbers[i] < 100
+      new_numbers << numbers[i]
+    end
+    i += 1
+  end
+  return new_numbers
+end
+
+numbers = [13, 113, 3, 33, 1133, 31]
+p less_than(numbers)
 
 # DEBUG
 
