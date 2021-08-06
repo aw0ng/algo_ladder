8da84fb9c601812f055dcd27a8e57bd31ee0d4d8
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-08-05 23:55:11 -0400

    map_double function

--- Basic Array/map_double.rb
+++ Basic Array/map_double.rb
@@ -4,10 +4,28 @@
 # Output: [8, 4, 10, 198, -8]
 
 # CLARIFY
+# array = [1, 2, 3, 4, 5]
+# new_array = [2, 4, 6, 8, 10]
 
 # VISUALIZE
+# array = [1, 2, 3, 4, 5]
+          # i * 2 >> new_array
+# new_array = [2, 4, 6, 8, 10]
 
 # CODE
+
+def double(numbers)
+  i = 0
+  new_array = []
+  while i < numbers.length
+    new_array << numbers[i] * 2
+    i += 1
+  end
+  return new_array
+end
+
+numbers = [1, 2, 3, 4, 5]
+p double(numbers)
 
 # DEBUG
 
