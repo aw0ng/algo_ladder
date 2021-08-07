2686a8ee5c0fe076d13cb97a87525744466cbb69
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-08-07 00:09:59 -0400

    array_max function

--- Basic Array/array_max.rb
+++ Basic Array/array_max.rb
@@ -7,7 +7,25 @@
 
 # VISUALIZE
 
+# Input: [5, 17, -4, 20, 12]
+# Output: 20
+
 # CODE
+
+def greatest(numbers)
+  greatest = 0
+  i = 0
+  while i < numbers.length
+    if numbers[i] > greatest
+      greatest = numbers[i]
+    end
+    i += 1
+  end
+  return greatest
+end
+
+numbers = [5, 17, -4, 20, 12]
+p greatest(numbers)
 
 # DEBUG
 
