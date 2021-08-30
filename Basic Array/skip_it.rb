22bbb4f4c240bbb821e729bf9c78921a1d7fe7a0
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-08-29 23:20:34 -0400

    skip_it

--- Basic Array/skip_it.rb
+++ Basic Array/skip_it.rb
@@ -2,17 +2,38 @@
 
 # The new array should always start with the first number from the original array. The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
 
+# CLARIFY
+
 # Input:
 # [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
 
 # Output:
 # [2, 3, 1, 2, 2, 1, 5, 2, 2]
 
-# CLARIFY
-
 # VISUALIZE
 
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+  #count
+      #1 #2
+         #count
+            #1 #2 #3
+
 # CODE
+
+def skip(numbers)
+  new_array = []
+  i = 0
+  while i < numbers.length
+    new_array << numbers[i]
+    count = numbers[i]
+    i += count
+  end
+  return new_array
+end
+
+numbers = [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+p skip(numbers)
 
 # DEBUG
 
