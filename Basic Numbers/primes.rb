a8b33d7736b791fd49f5e4fcdfd05086c7e18d9a
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-03-18 23:28:47 -0400

    primes

--- Basic Numbers/primes.rb
+++ Basic Numbers/primes.rb
@@ -1,37 +1,21 @@
 # Write a function that returns whether a given number is a prime number.
 
-# def is_prime(n)
-#   if n == 1
-#     return "#{n} is a prime number"
-#   elsif n % 2 == 0
-#     return "#{n} is a prime number"
-#   elsif
-
-#     i = n - 1
-#     while n > 1
-#       if n % i == 0
-#         return "#{n} is not a prime number"
-#       else
-#         return "#{n} is a prime number"
-#       end
-#       i -= 1
-#     end
-#   end
-# end
-
-# p is_prime(9)
-# p is_prime(3)
-
-def prime_number(number)
-  (2..(number-1)).each do |value|   
-    if (number % value) == 0
-      return false
+def is_prime(n)
+  if n < 2
+    return false
+  else
+    i = 2
+    while i < n
+      if n % i == 0
+        return false
+      end
+      i += 1
     end
-    return true
-  end    
-end
+  end
+  return true
+end 
 
-p prime_number(4)
-p prime_number(9)
-p prime_number(3)
-p prime_number(13)
+p is_prime(5)
+p is_prime(17)
+p is_prime(27)
+p is_prime(33)
