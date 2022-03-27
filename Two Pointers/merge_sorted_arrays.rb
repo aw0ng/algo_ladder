6bc746f431a874e51bc5caf9c8e6b92ca743fa16
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-03-26 21:33:34 -0400

    merge sorted arrays - diff solution

--- Two Pointers/merge_sorted_arrays.rb
+++ Two Pointers/merge_sorted_arrays.rb
@@ -27,3 +27,32 @@ end
 A = [1, 5, 8]
 B = [6, 9]
 p merge(A, B)
+
+c = [3, 4, 6, 10, 11, 15]
+d = [1, 5, 8, 12, 14, 19]
+
+def merge_arrays(c, d)
+  # build a holder array that is the size of both input arrays
+  # O(n) space
+  result = []
+
+  # get lower head value
+  if c[0] < d[0]
+    result << c.shift
+  else
+    result << d.shift
+  end
+
+  # check to see if either array is empty
+  if c.length == 0
+    return result + d
+  elsif d.length ==0
+    return result + c
+  else
+    return result + merge_arrays(c, d)
+  end
+      
+end
+p merge_arrays(c, d)
+# prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+p merge_arrays(A, B)
